152b1d1d4da034922d4c5497e91b16e4b3ec2eb0
Anatoly Piskunov <anatoly.piskunov@gmail.com>
2020-06-05 18:34:21 +0400

    JS init step

---
+++ app.js
@@ -0,0 +1,113 @@
+var level=0;
+var path='viz://';
+var search='';
+
+function app_mouse(){
+	if(!e)e=window.event;
+	var target=e.target || e.srcElement;
+	if(typeof $(target).attr('data-href') != 'undefined'){
+		var href=$(target).attr('data-href');
+		/*//change menu element state
+		if($(target).hasClass('menu-el')){
+			if('none'==$('.menu-list').css('float')){
+				$('.menu-list').css('display','none');
+			}
+		}
+		*/
+		view_path(href,{},true);
+		e.preventDefault();
+	}
+	if($(target).hasClass('go-top')){
+		$(window)[0].scrollTo({behavior:'smooth',top:0});
+	}
+}
+
+function app_keyboard(e){
+	if(!e)e=window.event;
+	var key=(e.charCode)?e.charCode:((e.keyCode)?e.keyCode:((e.which)?e.which:0));
+	let char=String.fromCharCode(key);
+	//console.log(key,char);
+	/*
+	if(key==27){
+		e.preventDefault();
+	}
+	*/
+}
+
+function parse_fullpath(){
+	let fullpath=window.location.hash.substr(1);
+	path='';
+	search='';
+	if(-1==fullpath.indexOf('?')){
+		path=fullpath;
+	}
+	else{
+		path=fullpath.substring(0,fullpath.indexOf('?'));
+		search=fullpath.substring(fullpath.indexOf('?')+1);
+	}
+	if(''==path){
+		path='viz://';
+	}
+}
+
+function view_path(location,state,save_state){
+	save_state=typeof save_state==='undefined'?false:save_state;
+	var path_parts=[];
+	var title='';
+
+	if(typeof state.path == 'undefined'){
+		if(-1!=location.indexOf('viz://')){
+			path_parts=location.substr(location.indexOf('viz://')+6).split('/');
+		}
+		else{
+			path_parts=location.split('/');
+		}
+	}
+	else{
+		path_parts=state.path;
+	}
+
+	//work with path_parts, form new view or update current
+	//clearTimeout(update_dgp_timer);
+	//$('.view').css('display','none');
+
+	if(save_state){
+		history.pushState({path,title},'','#'+location);
+	}
+
+	document.title=title;
+
+	console.log(location,path_parts);
+	if(''!=location){
+		if($('.view[data-path="'+location+'"]').length>0){
+			$('.view[data-path="'+location+'"]')[0].scrollIntoView({behavior:'smooth',block:'start'});
+		}
+	}
+	else{
+		$(window)[0].scrollTo({behavior:'smooth',top:0});
+	}
+}
+
+function escape_html(text) {
+	var map = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#039;'
+	};
+	return text.replace(/[&<>"']/g,function(m){return map[m];});
+}
+
+$(window).on('hashchange',function(e){
+	e.preventDefault();
+	parse_fullpath();
+	view_path(path,{},true);
+});
+
+parse_fullpath();
+view_path(path,{},true);
+
+document.addEventListener('click', app_mouse, false);
+document.addEventListener('tap', app_mouse, false);
+document.addEventListener('keyup', app_keyboard, false);
